46f7e2ecd2b584b59c3c9acaf92c35118a98a3ac
blackballfoo <foobass@googlemail.com>
2019-07-14 22:14:21 +0100

    Roman numerals - initial Commit.

---
+++ src/test/java/RomanNumeralTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class RomanNumeralTest {
+
+
+    @Test
+    public void test(){
+
+        assertThat("MWW",  equalTo("jkj"));
+    }
+}
